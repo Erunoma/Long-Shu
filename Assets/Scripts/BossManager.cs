34a4f41b79c91a2da5c6228c55a9983c5e986302
Oliver Boots <oliver.boots@hotmail.com>
2017-10-30 13:02:29 +0100

    1. Branch
    - Added framework for boss
    - Added animations for boss idle
    - Added animations for side lightning

---
+++ Assets/Scripts/BossManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossManager : MonoBehaviour {
+
+	public float health;
+	public int str;
+	public GameObject cloud;
+	public Animator anim;
+	public int phase;
+
+	public int activeAbility;
+	public int cooldown;
+
+	// Use this for initialization
+	void Start () {
+		health = 100;
+		InitBoss ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void InitBoss(){
+		phase = 1;
+		activeAbility = 1;
+	}
+}
