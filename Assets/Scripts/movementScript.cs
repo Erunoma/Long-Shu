228a211c4317d86fdcc169d4164b88d32894a60c
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2017-10-30 12:19:57 +0100

    player movement

---
+++ Assets/Scripts/movementScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour {
+		
+	public float speed;
+	private Rigidbody2D rb2d;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		Vector2 direction = transform.position;
+
+		rb2d.MovePosition(rb2d.position + movement*speed*Time.fixedDeltaTime);
+
+	
+	}
+}
