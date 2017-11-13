e75f4cea63ead790af5b6da82c97d0ceb38d9dc9
Marc Bisgaard Petersen <swittyxxz23@gmail.com>
2017-11-13 12:11:51 +0100

    sounds

--- Assets/Scripts/FloatingPlatform.cs
+++ Assets/Scripts/FloatingPlatform.cs
@@ -6,12 +6,14 @@ public class FloatingPlatform : MonoBehaviour {
 
 	public Transform target;
 	public Transform[] stops;
+	public GameObject player;
 	public int number;
 	public float step;
 	// Use this for initialization
 	void Start () {
 		target = stops[0];
 		number = 0;
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	// Update is called once per frame
@@ -23,6 +25,25 @@ public class FloatingPlatform : MonoBehaviour {
 			number %= stops.Length;
 			target = stops [number];
 		}
+		if (player.GetComponent<movementScript>().airborne) {
+			player.transform.parent = null;
+		}
 
 	}
+	void OnCollisionEnter2D (Collision2D other)//when player comes in contact with something 
+	{
+		if (other.gameObject.tag == "Player") {
+			//Sets "newParent" as the new parent of the player GameObject.
+			player.transform.SetParent(GameObject.Find("floating platform").transform);
+
+			//Same as above, except this makes the player keep its local orientation rather than its global orientation.
+			player.transform.SetParent(GameObject.Find("floating platform").transform,false);
+	
+		}
+	}
+	void	OnCollosionExit2D(Collision2D other)
+	{
+		player.transform.parent = null;
+	}
+
 }
